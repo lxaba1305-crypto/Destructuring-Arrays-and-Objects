3a5ac2eb3aff5fad5e86a24f0b086b3e4d61f88b
Lehlohonolo Xaba <your-email@example.com>
2026-02-11 17:01:59 +0200

    Added ppractice code

---
+++ script.js
@@ -0,0 +1,54 @@
+const fruits = ["apple", "banana", "cherry"]
+
+const [firstFruit, secondFruit, thirdFruit] = fruits
+
+console.log(firstFruit) // Output: "apple"
+console.log(secondFruit) // Output: "banana"
+console.log(thirdFruit) // Output: "cherry"
+
+const car = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2020
+}
+
+const { make, model, year } = car
+
+console.log(make) // Output: "Toyota"
+console.log(model) // Output: "Camry"
+console.log(year) // Output: 2020
+
+const colors = ["red"];
+
+const [
+    primaryColor,
+    secondaryColor = "green",
+    tertiaryColor = "blue"
+] = colors;
+
+console.log(primaryColor); // Output: "red"
+console.log(secondaryColor); // Output: "green"
+console.log(tertiaryColor); // Output: "blue"
+
+const user = {
+  name: "Blessing",
+  age: 19,
+  address: {
+    city: "Johannesburg",
+    zip: "2001"
+  }
+};
+
+const {
+  name: userName,
+  age: userAge,
+  address: {
+    city: userCity,
+    zip: userZip
+  }
+} = user;
+
+console.log(userName);
+console.log(userAge);
+console.log(userCity);
+console.log(userZip);
